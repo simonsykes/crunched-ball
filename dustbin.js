580950ddcf6f617ccdc2b6f24425f118b28c4721
simonsykes <69767319+simonsykes@users.noreply.github.com>
2020-09-30 21:03:33 -0500

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,37 @@
+class dustbin{
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+        this.image=loadImage("dustbingreen.png");
+
+        this.bottomBody=Bodies.rectangle(this.x,this.y,200,20,{isStatic:true});
+        this.leftBody=Bodies.rectangle(this.x-100,this.y-107,20,213,{isStatic:true});
+        this.rightBody=Bodies.rectangle(this.x+100,this.y-107,20,213,{isStatic:true});
+
+        World.add(world,this.bottomBody); 
+        World.add(world,this.leftBody);
+        World.add(world,this.rightBody);    
+    }
+    display(){
+        push();
+        translate(this.leftBody.position.x,this.leftBody.position.y);
+        rectMode(CENTER);
+        angleMode(RADIANS);
+        rotate(this.angle);
+        pop();
+
+        push();
+        translate(this.rightBody.position.x,this.rightBody.position.y);
+        rectMode(CENTER);
+        angleMode(RADIANS);
+        rotate(-1*this.angle);
+        pop();
+
+        push();
+        translate(this.bottomBody.position.x,this.bottomBody.position.y+10);
+        imageMode(CENTER);
+        angleMode(RADIANS);
+        image(this.image,0,-107,200,107);
+        pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body; 
+
+var dustbinObj,paperObj,groundObj;
+var world,engine;
+
+
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paperObj=new paper(200,450,70);
+	groundObj=new ground(800,670,1600,20);
+	dustbinObj=new dustbin(1200,650);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+	
+  paperObj.display();
+  groundObj.display();
+  dustbinObj.display();
+  
+ 
+}
+
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paperObj.body,paperObj.body.position,{x:130,y:-145});
+	}
+}
+
+
+
